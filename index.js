3d1fc3998e87d2317a98cdd865214d78a14157b4
iammsoheb-cpu <iammsoheb@gmail.com>
2025-11-18 16:10:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
+const { Client, GatewayIntentBits, Routes } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages
+  ],
+});
+
+const commands = [
+  {
+    name: "submit",
+    description: "Submit a link",
+    options: [
+      {
+        name: "link",
+        type: 3,
+        description: "The link you want to submit",
+        required: true,
+      },
+    ],
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+    console.log("Slash command /submit is ready!");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on("ready", () => {
+  console.log(`Bot logged in as ${client.user.tag}`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "submit") {
+    const link = interaction.options.getString("link");
+
+    const channel = interaction.guild.channels.cache.find(
+      (c) => c.name === "submissions"
+    );
+
+    if (!channel)
+      return interaction.reply("Channel **#submissions** not found!");
+
+    channel.send(`📌 **New Link Submitted:** ${link}`);
+    interaction.reply("Your link has been submitted 😊");
+  }
+});
+
+client.login(process.env.TOKEN);
 
